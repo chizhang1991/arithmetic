de32ad162684df154b1da0d94f192eb2fa09bdc7
chizhang1991 <gongchangchi@hotmail.com>
2016-06-22 19:13:06 +0800

    reverse.java input

--- stackqueues/reverse.java
+++ stackqueues/reverse.java
@@ -73,5 +73,21 @@ class ReverseApp {
 		String output;
 
 		System.out.println("Enter a string: ");
+		System.out.flush();
+		input = getString();
+		if(input.equals(""))
+			break;
+
+		Reverser theReverser = new Reverser(input);
+		output = theReverser.doRev();
+		System.out.println("Reversed: " + output);
+	}
+//-------------------------------------
+	public static String getString() 
+	throws IOException {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		String s = br.readLine();
+		return s;
 	}
 }
